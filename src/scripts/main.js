8acdbfd5cdc485a3b0bce299a8993cc4bb5a0cba
bondydaa <bondydaa@gmail.com>
2014-03-19 22:14:18 -0500

    not much done on paper but logic worked out in my head

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,18 +12,25 @@ var suggestion = {
         q: query
       },
       success: function(res, status){
-        self.displayResult(res);
+        self.displayResult(res, query);
       }
     });
   },
-  displayResult: function(apiCallBack){
+  displayResult: function(apiCallBack, query){
     var response = apiCallBack,
         i = 0,
         template = $('#result-display').html(),
         compiled = _.template(template, response.movies[i]);
 
     $('.results').html(compiled);
-    console.log(compiled, response.movies[i]);
+    console.log(response, query);
+    this.differentResult(response, query);
+  },
+  differentResult: function(reponse, query){
+    // setTimeout(1000);
+    $('#prev, #next').on('click', function(e){
+      alert(this+' was clicked');
+    });
   },
   ratingSuggest: function(){
 
@@ -33,9 +40,9 @@ var suggestion = {
 
 
 $formControl.on('submit', function(e){
-  var inputVal = $movieInput.val();
+  var $inputVal = $movieInput.val();
 
-  suggestion.processQuery(inputVal);
+  suggestion.processQuery($inputVal);
 
 
   $movieInput.val('');
